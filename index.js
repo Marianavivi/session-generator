db2b75687d8601dd7ff24bdd62f8fa8e9a66460b
Mariana <marianagutierrezvivi@gmail.com>
2024-11-13 16:09:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`YoutTube: @GlobalTechInfo\n\nGitHub: @GlobalTechInfo\n\nServer running on http://localhost:${PORT}`);
+    console.log(`YoutTube: @MarianaGutierrez-b5p\n\nGitHub: @Marianavivi\n\nServer running on http://localhost:${PORT}`);
 });
 
 export default app;
